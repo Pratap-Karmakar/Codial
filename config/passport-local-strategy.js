51017dfa5519d438285279295cb9500b7fdc94d9
Pratap-Karmakar <pratap2501.pk@gmail.com>
2022-10-21 21:17:14 +0530

    updated

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,8 +1,8 @@
 // import passport
-const passport=require('passport');
+const passport = require('passport');
 
 //import passport-local
-const LocalStrategy=require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 
 // import user
@@ -15,21 +15,33 @@ const User = require('../models/user');
 // authentication using passport
 passport.use(new LocalStrategy({
     usernameField: 'email'
-    },
-    function(email,password,done){
+},
+    function (email, password, done) {
         // fin an user and publish the identity 
-        User.findOne({email:email},
-            function(err,user){
-                if(err){
+        User.findOne({ email: email },
+            function (err, user) {
+                if (err) {
                     console.log('Error in finding user --> Passport');
                     return done(err);
                 }
-                if(!user || user.password!= password){
+                if (!user || user.password != password) {
                     console.log('Invalid USername/PAssword');
                     // this return means, that there is no error but no user is found.
-                    return done(null,false);
+                    return done(null, false);
                 }
-                return done(null,false);
+                return done(null, false);
             })
     }
-))
+));
+
+
+
+
+// serializing the user to decide which key is to be kept in the cookie.
+
+
+
+
+
+
+// deserializing the user from the key in the cookies
